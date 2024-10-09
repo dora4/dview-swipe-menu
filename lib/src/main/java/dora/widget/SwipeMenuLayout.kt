ece48931d9b3c28fec9ddeff4c66ce8875b5da66
dora <dora924666990@gmail.com>
2024-10-09 14:58:53 +0800

    侧滑菜单

--- lib/src/main/java/dora/widget/SwipeMenuLayout.kt
+++ lib/src/main/java/dora/widget/SwipeMenuLayout.kt
@@ -14,6 +14,7 @@ import android.view.ViewConfiguration
 import android.view.ViewGroup
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.OvershootInterpolator
+import dora.widget.swipemenu.R
 import kotlin.math.abs
 
 /**
